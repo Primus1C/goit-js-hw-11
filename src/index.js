5c865d431aa2b9de16b0adb36281f9a855483041
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-02 13:53:56 +0200

    develop

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const axios = require('axios');
+const apiKey = '6095343-d47de4ae86d54fd6f681d759d';
+const baseURL = 'https://pixabay.com/api/';
+const baseType = 'image_type=photo';
