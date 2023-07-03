13d90fc42876cc6ee2012b1cb1df370021e26717
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-03 21:10:01 +0200

    develop

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -29,18 +29,3 @@ export class PixabayAPI {
   }
 
 }
-  
-/* export async function getImages(strQuery) {
-  const resp = await axios
-    .get(
-      `${BASE_URL}/?key=${API_KEY}&q=${strQuery}&${BASE_TYPE}&orientation=horizontal&safesearch=true`
-    )
-    .then(response => {
-      console.log('response', response);
-      return response.data;
-    })
-    .catch(error => {
-      //console.log(error);
-      Notiflix.Notify.failure(error.message);
-    });
-} */
