b48a6a874728d6b54b1c805616c31a30caa182f0
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-05 16:11:35 +0200

    feedback

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Notiflix from 'notiflix';
 import { PixabayAPI } from './js/PixabayAPI';
 import createGalleryCard from '../templates/gallery-card.hbs';
 
+const PER_PAGE = 40;
 const refs = {
   form: document.querySelector('.search-form'),
   input: document.querySelector('input[name="searchQuery"]'),
@@ -15,13 +16,15 @@ const refs = {
 refs.form.addEventListener('submit', onFormSubmit);
 refs.buttonNext.addEventListener('click', onClickNext);
 
-let curPage = 1;
+let curPage = 0;
 
 showElement(refs.containerNext, false);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
+  curPage = 1;
   renderPage(curPage, true);
+  //refs.input.value = '';
 }
 
 function onClickNext(evt) {
@@ -31,39 +34,44 @@ function onClickNext(evt) {
 
 async function renderPage(page, itsFirstQuery) {
   const pixabayAPI = new PixabayAPI(page);
-  const queryStr = refs.input.value.replace(/ /g, '+');
-  const response = await pixabayAPI
-    .getImages(queryStr)
-    .then(resp => {
-      const totalPages = Math.ceil(Number(resp.data.totalHits) / 40);
-      if ((totalPages > 0) && (itsFirstQuery===true)) {
-        Notiflix.Notify.success(
-          `Hooray! We found ${resp.data.totalHits} images.`
-        );
-      };
-      const arrData = createGalleryCard(resp.data.hits);
-      if (arrData.length === 0) {
-        Notiflix.Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      };
-      if (page > totalPages) {
+  const queryStr = refs.input.value.trim().replace(/ /g, '+');
+  if (queryStr === '' && itsFirstQuery) {
+    return;
+  }
+  console.log('Page', page);
+  try {
+    const response = await pixabayAPI.getImages(queryStr);
+    const totalPages = Math.ceil(Number(response.data.totalHits) / PER_PAGE);
+    if (totalPages > 0 && itsFirstQuery === true) {
+      //console.log(response.data);
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
+      );
+    }
+    const arrData = createGalleryCard(response.data.hits);
+    if (arrData.length === 0) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (page === totalPages) {
+      if (page > 1) {
         Notiflix.Notify.info(
           "We're sorry, but you've reached the end of search results"
         );
-        showElement(refs.containerNext, false);
-        return;
       }
+      showElement(refs.containerNext, false);
+    } else {
       showElement(refs.containerNext);
-      refs.gallery.innerHTML = arrData;
-      const lightbox = new SimpleLightbox('.gallery a', {
-        captionDelay: 250,
-      });
-    })
-    .catch(err => {
-      Notiflix.Notify.failure(err.message);
+    }
+    refs.gallery.innerHTML = arrData;
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
     });
+  } catch (err) {
+    Notiflix.Notify.failure(err.message);
+  }
 }
 
 function showElement(element, show = true) {
